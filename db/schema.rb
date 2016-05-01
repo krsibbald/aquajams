55a220d6974486825dd63233f1afa59aba77dcb5
Katharine <krsibbald@gmail.com>
2016-04-30 22:08:11 -0400

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501015424) do
+ActiveRecord::Schema.define(version: 20160501015926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,4 +30,18 @@ ActiveRecord::Schema.define(version: 20160501015424) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "artist_id"
+    t.integer  "length_in_sec"
+    t.integer  "year"
+    t.integer  "top_billboard_spot"
+    t.decimal  "billboard_weeks"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "songs", ["artist_id"], name: "index_songs_on_artist_id", using: :btree
+
+  add_foreign_key "songs", "artists"
 end
