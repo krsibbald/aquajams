23cfee62f9c0d9234f19130c14f78dfefeb66163
Katharine <krsibbald@gmail.com>
2016-04-30 22:19:31 -0400

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501015926) do
+ActiveRecord::Schema.define(version: 20160501021903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,18 @@ ActiveRecord::Schema.define(version: 20160501015926) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "mixes", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "length_in_sec"
+    t.date     "recorded_at"
+    t.text     "description"
+    t.text     "source"
+    t.text     "music_type"
+    t.text     "notes"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
   create_table "songs", force: :cascade do |t|
     t.string   "name"
     t.integer  "artist_id"
@@ -43,5 +55,18 @@ ActiveRecord::Schema.define(version: 20160501015926) do
 
   add_index "songs", ["artist_id"], name: "index_songs_on_artist_id", using: :btree
 
+  create_table "tracks", force: :cascade do |t|
+    t.integer  "mix_id"
+    t.integer  "song_id"
+    t.integer  "ord"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tracks", ["mix_id"], name: "index_tracks_on_mix_id", using: :btree
+  add_index "tracks", ["song_id"], name: "index_tracks_on_song_id", using: :btree
+
   add_foreign_key "songs", "artists"
+  add_foreign_key "tracks", "mixes"
+  add_foreign_key "tracks", "songs"
 end
