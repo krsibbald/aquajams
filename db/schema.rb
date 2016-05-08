3a2af2ff4abe604199546a94285175474f61d593
Katharine <krsibbald@gmail.com>
2016-05-07 23:16:53 -0400

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508025812) do
+ActiveRecord::Schema.define(version: 20160508031536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20160508025812) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "cd_id"
+    t.integer  "bpm"
   end
 
   add_index "songs", ["artist_id"], name: "index_songs_on_artist_id", using: :btree
