148dd451b6f93d432aab764a7beb601595a9c5ac
Katharine <krsibbald@gmail.com>
2016-05-07 21:57:23 -0400

    initial spec for song import

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "import" do
+    let(:csv){"#{Rails.root}/spec/support/songs_for_import.csv"}
+    it "should import" do
+      expect(Song.count).to eq 0
+      Song.import(csv)
+      expect(Song.count).to eq 10
+
+      blues_song = Song.where(name: 'Summertime Blues').first
+      expect(blues_song).to_not be_nil
+      expect(blues_song.time_in_sec).to eq 118
+      expect(blues_song.artist.try(:name)).to eq "Eddie Cochran"
+      expect(blues_song.year).to eq 1958
+      
+    end
+  end
 end
