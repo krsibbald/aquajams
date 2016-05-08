47cb5587046dc453c1d9545af1bd08d126ebe9d4
Katharine <krsibbald@gmail.com>
2016-05-07 22:36:24 -0400

    Updated importer spec

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -7,14 +7,20 @@ RSpec.describe Song, type: :model do
     it "should import" do
       expect(Song.count).to eq 0
       Song.import(csv)
-      expect(Song.count).to eq 10
+      expect(Song.count).to eq 20
 
       blues_song = Song.where(name: 'Summertime Blues').first
       expect(blues_song).to_not be_nil
-      expect(blues_song.time_in_sec).to eq 118
+      expect(blues_song.length_in_sec).to eq 118
       expect(blues_song.artist.try(:name)).to eq "Eddie Cochran"
       expect(blues_song.year).to eq 1958
-      
+
+      stuck_song = Song.where(name: 'Stuck With You').first
+      expect(stuck_song).to_not be_nil
+      expect(stuck_song.artist.try(:name)).to eq "Huey Lewis And the News"
+      expect(stuck_song.year).to eq 1986
+      expect(stuck_song.top_billboard_spot).to eq 1
+      expect(stuck_song.billboard_weeks).to eq 1.03
     end
   end
 end
