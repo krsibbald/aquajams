9970c6d0bc916245ea6b9af5eff2afe29ca5c5f7
Katharine <krsibbald@gmail.com>
2016-05-08 21:37:25 -0400

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509010236) do
+ActiveRecord::Schema.define(version: 20160509011449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,11 @@ ActiveRecord::Schema.define(version: 20160509010236) do
     t.text     "source"
     t.text     "music_type"
     t.text     "notes"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+    t.integer  "code"
+    t.boolean  "multiple",          default: false, null: false
+    t.date     "date_for_mix_list"
   end
 
   create_table "songs", force: :cascade do |t|
