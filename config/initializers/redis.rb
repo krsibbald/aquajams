68b3f77dc40f26b33dc97fd0c0656d702d65487c
Katharine <krsibbald@gmail.com>
2016-05-16 22:36:05 -0400

    Revert config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,2 @@
-if ENV["REDISTOGO_URL"]
-  uri = URI.parse(ENV["REDISTOGO_URL"])
-  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-else
-  uri = "redis://localhost:6379/"
-  REDIS = Redis.new(:url => uri)
-end
+uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
+REDIS = Redis.new(:url => uri)
