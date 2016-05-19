abe015ee30dfecd06cb336816008b96820d3d1c9
Katharine <krsibbald@gmail.com>
2016-05-18 22:59:50 -0400

    Lowercase Aws in v2. Not AWS

--- app/controllers/mixes_controller.rb
+++ app/controllers/mixes_controller.rb
@@ -85,7 +85,7 @@ class MixesController < ApplicationController
         else
           remote_filename =  SecureRandom.hex + '.csv'
 
-          s3 = AWS::S3.new(
+          s3 = Aws::S3.new(
             access_key_id: ENV['AWS_ACCESS_KEY_ID'],
             secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
           )
