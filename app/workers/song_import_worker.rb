d8273febba2b13a008516b6ec749934089d2fffa
Katharine <krsibbald@gmail.com>
2016-06-03 23:00:22 -0400

    Move logic into workers

--- app/workers/song_import_worker.rb
+++ app/workers/song_import_worker.rb
@@ -1,7 +1,71 @@
+require 'csv'
+
 class SongImportWorker
   include Sidekiq::Worker
+  HEADERS = ["CD's ID", "___Song's Name", "Time, as hh:mm:ss", '___Year', '___Artist', '___Billboard Position', '__+ Weeks at', 'BPM 1']
 
   def perform(file)
-    Song.import(file)
+    row_num = 0
+    cd = nil
+    headers = []
+    file_name = file.respond_to?(:path) ? file.path : file
+    song_count = 0
+    cd_count = 0
+    artist_count = 0
+    CSV.foreach(file_name, 'r:ISO-8859-15:UTF-8') do |row|
+      row_num += 1
+      if row_num == 1
+        headers = row
+      else
+        info = Hash[*headers.zip(row).flatten] 
+        if ! info["CD's ID"].blank?  #this row has cd information
+          cd_code = info["CD's ID"]
+          cd = Cd.where(code: cd_code).first
+          unless cd
+            cd_name = info["___Song's Name"]
+            cd = Cd.where(name: cd_name).first
+            unless cd
+              cd = Cd.new(name: cd_name, code: cd_code)
+              if cd.save
+                cd_count += 1
+              end
+            end
+          end
+        else #this row has song information
+          song_name = info["___Song's Name"]
+          if cd #&& cd.songs.where(name: song_name).none?
+            s = Song.new
+            s.name = song_name
+            t = info["Time, as hh:mm:ss"]
+            if t && t.match(/\A[\d]?\d:\d\d:\d\d\z/)
+              t_arr = t.split(':').map(&:to_i)
+              s.length_in_sec = (((t_arr[0] * 60) + t_arr[1]) * 60) + t_arr[2] #hours + mins + seconds
+            end
+            s.year = info['___Year']
+
+            artist_name = info['___Artist'].try(:strip)
+            unless artist_name.blank?
+              artist = Artist.find_by_name artist_name
+              unless artist
+                artist = Artist.new(name: artist_name)
+                if artist.save
+                  artist_count += 1
+                end
+              end
+              s.artist = artist
+            end
+
+            s.top_billboard_spot = info['___Billboard Position']
+            s.billboard_weeks = info['__+ Weeks at']
+            s.cd_id = cd.try(:id) #cd is set during cd row processing
+            s.bpm = info['BPM 1']
+            if s.save!
+              song_count += 1
+            end
+          end
+        end
+      end
+    end
+    "#{song_count} Songs, #{cd_count} CDs, and #{artist_count} Artists added"
   end
 end
---
+++ spec/workers/song_import_worker_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+describe SongImportWorker do
+  
+  describe "import" do
+    let(:csv){"#{Rails.root}/spec/support/songs_for_import.csv"}
+    let!(:cd1958){FactoryGirl.create :cd, name: "1958, The Rock 'n' Roll Era", code: 1}
+    it "should import" do
+      expect(Song.count).to eq 0
+      expect(Cd.count).to eq 1
+
+      siw = SongImportWorker.new
+      siw.perform(csv)
+
+      expect(Song.count).to eq 18
+      expect(Cd.count).to eq 2
+    end
+    it "new artist, cd exists" do
+      siw = SongImportWorker.new
+      siw.perform(csv)
+
+      blues_song = Song.where(name: 'Summertime Blues').first
+      expect(blues_song).to_not be_nil
+      expect(blues_song.length_in_sec).to eq 118
+      expect(blues_song.artist.try(:name)).to eq "Eddie Cochran"
+      expect(blues_song.year).to eq 1958
+      expect(blues_song.bpm).to eq 157
+      expect(blues_song.cd.try(:name)).to eq "1958, The Rock 'n' Roll Era"
+    end
+    it "artist already exists, new cd" do
+      FactoryGirl.create :artist, name: 'Huey Lewis And the News'
+      
+      siw = SongImportWorker.new
+      siw.perform(csv)
+
+      stuck_song = Song.where(name: 'Stuck With You').first
+      expect(stuck_song).to_not be_nil
+      expect(stuck_song.artist.try(:name)).to eq "Huey Lewis And the News"
+      expect(stuck_song.year).to eq 1986
+      expect(stuck_song.top_billboard_spot).to eq 1
+      expect(stuck_song.billboard_weeks).to eq 1.03
+      expect(stuck_song.bpm).to eq 120
+      expect(stuck_song.cd.try(:name)).to eq "1986, Billboard Top Hits"
+    end
+    it "artist, cd, song already exists" do
+      sil = FactoryGirl.create :artist, name: 'Silhouettes'
+      FactoryGirl.create :song, name: 'Get a Job', artist: sil, cd: cd1958
+      
+      siw = SongImportWorker.new
+      siw.perform(csv)
+
+      expect(Song.where(name: 'Get a Job').count).to eq 1
+      expect(Cd.where(name: "1958, The Rock 'n' Roll Era").count).to eq 1
+      expect(Artist.where(name: 'Silhouettes').count).to eq 1
+      job_song = Song.where(name: 'Get a Job').first
+      expect(job_song).to_not be_nil
+      expect(job_song.artist.try(:name)).to eq "Silhouettes"
+      expect(job_song.cd.try(:name)).to eq "1958, The Rock 'n' Roll Era"
+    end
+    it "creates cds" do
+      siw = SongImportWorker.new
+      siw.perform(csv)
+
+      bad_song = Song.where(name: '1986, Billboard Top Hits').first
+      expect(bad_song).to be_nil
+      good_cd = Cd.where(name: '1986, Billboard Top Hits').first
+      expect(good_cd).to_not be_nil
+    end
+  end
+end
