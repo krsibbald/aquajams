2b5f713df938107f6018deeaba01dd54655b5454
Katharine <krsibbald@gmail.com>
2016-06-16 10:57:19 -0400

    Update mix importer worker to deal with existing mixes

--- app/workers/mix_import_worker.rb
+++ app/workers/mix_import_worker.rb
@@ -19,31 +19,29 @@ class MixImportWorker < ImportWorker
         info = Hash[*headers.zip(row).flatten] 
 
         mix_code = info["___Prime, RoundDown (2)"]
-        if Mix.where(code: mix_code).none?
-          m = Mix.new(code: mix_code)
-          m.multiple = (info['One or Multiple?'] == 'm')
-          ttime = info['__Total Time']
-          m.length_in_sec = (ttime.to_i * 60) unless ttime.blank?
-          mix_list_date_str = info['___"Mix List" ID:  CD_mm-dd-yy.xls.']
-          unless mix_list_date_str.blank?
-            date_arr = mix_list_date_str.split('/').map(&:to_i)
-            d = Date.new((2000 + date_arr[2]), date_arr[0], date_arr[1])
-            m.date_for_mix_list = d
-          end
+        m = Mix.where(code: mix_code).first || Mix.new(code: mix_code)
+        m.multiple = (info['One or Multiple?'] == 'm')
+        ttime = info['__Total Time']
+        m.length_in_sec = (ttime.to_i * 60) unless ttime.blank?
+        mix_list_date_str = info['___"Mix List" ID:  CD_mm-dd-yy.xls.']
+        unless mix_list_date_str.blank?
+          date_arr = mix_list_date_str.split('/').map(&:to_i)
+          d = Date.new((2000 + date_arr[2]), date_arr[0], date_arr[1])
+          m.date_for_mix_list = d
+        end
 
-          recorded_date_string = info['Date Recorded']
-          unless recorded_date_string.blank?
-            date_arr = recorded_date_string.split('/').map(&:to_i)
-            d = Date.new((2000 + date_arr[2]), date_arr[0], date_arr[1])
-            m.recorded_at = d
-          end
-          m.description = info['___Songs']
-          m.source = info['Source']
-          m.music_type = info['___Music Type']
-          m.notes = info['___Remarks']
-          if m.save
-            mix_count += 1
-          end
+        recorded_date_string = info['Date Recorded']
+        unless recorded_date_string.blank?
+          date_arr = recorded_date_string.split('/').map(&:to_i)
+          d = Date.new((2000 + date_arr[2]), date_arr[0], date_arr[1])
+          m.recorded_at = d
+        end
+        m.description = info['___Songs']
+        m.source = info['Source']
+        m.music_type = info['___Music Type']
+        m.notes = info['___Remarks']
+        if m.save
+          mix_count += 1
         end
       end
     end
